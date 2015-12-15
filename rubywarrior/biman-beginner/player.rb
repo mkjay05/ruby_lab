774eef63086f8d169633674e63d9c375dc2e3e6c
mkjay05 <chen.sijay@gmail.com>
2015-12-14 21:33:50 -0800

    level five

--- rubywarrior/biman-beginner/player.rb
+++ rubywarrior/biman-beginner/player.rb
@@ -1,29 +1,33 @@
-# Store the last known health in the instance variable @last_known_health
-# use this variable to see if we're taking damage
+
 class Player
-  MIN_HEALTH = 15
-  
+MIN_HEALTH = 15
   def play_turn(warrior)
-    if warrior.feel.empty?
-      if should_rest? warrior
-        warrior.rest!
-      else
+
+    # cool code goes here
+    if warrior.feel.empty? 
+      
+        if should_rest? (warrior)
+          warrior.rest!
+        else
+
         warrior.walk!
       end
-    else
-      warrior.attack!
+    elsif warrior.feel.captive?
+        warrior.rescue!
+    else 
+        warrior.attack!
     end
     
     @last_known_health = warrior.health
   end
-  
-  private
-  
-  def should_rest? warrior
-    # Don't worry too much about the next line
-    @last_known_health = warrior.health unless @last_known_health
-    safe = warrior.health >= @last_known_health
-    bad_health = warrior.health < MIN_HEALTH
-    safe && bad_health
+
+  def should_rest? (warrior)
+      # Following two lines are the same
+      # @last_known_health ||= warrior.health
+      @last_known_health = warrior.health unless @last_known_health 
+      # warrior.health  < MIN_HEALTH && (@last_known_health <= warrior.health)
+      safe = @last_known_health <= warrior.health
+      bad_health = warrior.health  < MIN_HEALTH
+      safe && bad_health 
   end
 end
