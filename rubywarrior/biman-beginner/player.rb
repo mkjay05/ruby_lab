af0abf67485b5d44d53e08ff57c3f096ae0a6c5b
mkjay05 <chen.sijay@gmail.com>
2015-12-18 22:33:46 -0800

    level six

--- rubywarrior/biman-beginner/player.rb
+++ rubywarrior/biman-beginner/player.rb
@@ -1,33 +1,46 @@
-
 class Player
-MIN_HEALTH = 15
+  MIN_HEALTH = 15
+  FLEE_HEALTH = 7
+  
   def play_turn(warrior)
-
-    # cool code goes here
-    if warrior.feel.empty? 
-      
-        if should_rest? (warrior)
-          warrior.rest!
-        else
-
-        warrior.walk!
+    # switch to ||= like a good boy
+    @last_known_health ||= warrior.health
+    @direction ||= :forward
+    feel_space = warrior.feel @direction
+    
+    if feel_space.empty?
+      if should_flee? warrior
+        @direction = :backward
+        warrior.walk! @direction
+      elsif should_rest? warrior
+        warrior.rest!
+      else
+        warrior.walk! @direction
       end
-    elsif warrior.feel.captive?
-        warrior.rescue!
-    else 
-        warrior.attack!
+    elsif feel_space.captive?
+      warrior.rescue! @direction
+    elsif feel_space.wall?
+      @direction = :forward
+    else
+      warrior.attack!
     end
     
     @last_known_health = warrior.health
   end
-
-  def should_rest? (warrior)
-      # Following two lines are the same
-      # @last_known_health ||= warrior.health
-      @last_known_health = warrior.health unless @last_known_health 
-      # warrior.health  < MIN_HEALTH && (@last_known_health <= warrior.health)
-      safe = @last_known_health <= warrior.health
-      bad_health = warrior.health  < MIN_HEALTH
-      safe && bad_health 
+ 
+  private
+  
+  def should_flee? warrior
+    bad_health = warrior.health < FLEE_HEALTH
+    !safe?(warrior) && bad_health
+  end
+  
+  def should_rest? warrior
+    bad_health = warrior.health < MIN_HEALTH
+    safe?(warrior) && bad_health
+  end
+  
+  def safe? warrior
+    warrior.health >= @last_known_health
   end
 end
